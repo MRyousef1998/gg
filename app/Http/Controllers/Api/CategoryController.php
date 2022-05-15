bbb493fad409b4965ad96e811bf68d088209205c
MRyousef1998 <75533977+MRyousef1998@users.noreply.github.com>
2022-05-16 00:13:31 +0300

    Update CategoryController.php

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -11,11 +11,16 @@ class CategoryController extends Controller
 {
     
         public function index(){
-            return CategoryResource::collection( Category::paginate(15)) ;
+            return CategoryResource::collection( Category::all()) ;
 
         }
         public function show($id){
             return new CategoryResource(Category::find($id));
+         }
+         public function product($id){
+             $category=Category::find($id);
+             return $category->products()->paginate(15);
+            
          }
 
 
