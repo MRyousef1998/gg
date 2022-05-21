b3e0bf7261324792a478fb17e7ec980ada78aab9
MRyousef1998 <75533977+MRyousef1998@users.noreply.github.com>
2022-05-21 03:48:44 +0300

    Update ImageFactory.php

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Image::class, function (Faker $faker) {
     return [
-        'url'=>$this->faker->imageUrl(640, 480, 'animals', true);,
+        'url'=>$this->faker->imageUrl(640, 480, 'animals', true),
             'product_id'=>$this->faker->numberBetween(1,1500),
            
     ];
