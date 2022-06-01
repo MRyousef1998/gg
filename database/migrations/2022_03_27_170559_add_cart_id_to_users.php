a8f937e4a01364bbe892055d3fa028e00c978bb6
MRyousef1998 <75533977+MRyousef1998@users.noreply.github.com>
2022-06-02 02:32:05 +0300

    Update 2022_03_27_170559_add_cart_id_to_users.php

--- database/migrations/2022_03_27_170559_add_cart_id_to_users.php
+++ database/migrations/2022_03_27_170559_add_cart_id_to_users.php
@@ -1,1 +1,34 @@
 
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCartIdToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->bigInteger('cart_id')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+         $table->dropColumn('cart_id');
+        });
+    }
+}
