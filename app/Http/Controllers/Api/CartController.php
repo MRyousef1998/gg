a90324691b333385e44744a040045ec8c91b1e3e
MRyousef1998 <75533977+MRyousef1998@users.noreply.github.com>
2022-06-03 22:42:11 +0300

    Update CartController.php

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -10,16 +10,33 @@ use  App\Cart;
 class CartController extends Controller
 {
     
-        public function index(Request $request){
+        public function AddProductToCart(Request $request){
+            
+            $request->validate([
+               ' product_id' => 'required',
+                'qty'=>'required]);
             $user=Auth::user();
-            $cart=$this->conectionState($user->cart);
+            
+            $product_id=$request.input('product_id');
+            $product=Product::findOrFall($product_id);
+            $cart=$this->cartState($user->cart);
+            $qty=$request->input('qty');
+            if($cart->inItem($product_id)){
+             $cart->incrmentProductinCart($product);
+            }
+            else{
+               $cart->addProductToCart($product,$qty);
+            }
+             $cart->save();
+            return new CartResource($cart);
 
         }
-    private function conectionState(Cart $cart){
+    private function cartState(Cart $cart){
         if(is_null($cart)){
             $cart=new Cart();
             $cart->cart_item=[];
             $cart->total=0;
+           
             return $cart;
         }
         return $cart;
