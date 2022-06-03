e0e1590ff31215311d271c332772d1ea6d918caa
MRyousef1998 <75533977+MRyousef1998@users.noreply.github.com>
2022-06-03 22:44:25 +0300

    Update Cart.php

--- app/Cart.php
+++ app/Cart.php
@@ -19,11 +19,37 @@ class Cart extends Model
        public function order(){
         return $this->belongsToMany(Order::class);
     }
-       public function cartItem(){
-        return $this->belongsToMany(Order::class);
+       public function cartItems(){
+           if(is_null($this->cartItems)){
+           $this->cartItems=[];
+              return $this->cartItems; 
+        }
+        return json_decode($this->cartItems);
+    }
+     public function addProductToCart(Product $product,$qty=1){
+         $cartItems=$this->cartItems();
+         $cartItem=new CartItem($product,$qty); 
+         array_push($cartItems,$cartItem);
+        
+    }
+    
+    public function incrmentProductinCart(Product $product,$qty=1){
+          $cartItems=$this->$cartItems();
+          foreach($cartItems as $cartItem){
+          if($productId==$cartItem->product->id){
+              $cartItem->qty+=$qty;
+          
+          }
     }
    
-      public function inItem(){
-        return $this->belongsToMany(Order::class);
+      public function inItem($productId){
+          $cartItems=$this->$cartItem();
+          foreach($cartItems as $cartItem){
+          if($productId==$cartItem->product->id){
+              return true;
+          
+          }
+          }
+          return false;
       }
 }
