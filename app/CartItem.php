b680fe114d33443938cb58344726d5e51d6d5bf7
MRyousef1998 <75533977+MRyousef1998@users.noreply.github.com>
2022-06-03 22:37:31 +0300

    Update CartItem.php

--- app/CartItem.php
+++ app/CartItem.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+   public $product;
+   public $qtr;  
+    public function _construct(Product $product,$qtr){
+    $this->product=$product;
+        $this->qty=$qtr;
+    
+    }
+    
+} 
