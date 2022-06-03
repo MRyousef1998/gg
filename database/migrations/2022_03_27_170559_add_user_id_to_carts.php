159ec7e8c219d561cd1d6bac0730d89cdba509dd
MRyousef1998 <75533977+MRyousef1998@users.noreply.github.com>
2022-06-04 02:37:08 +0300

    Update 2022_03_27_170559_add_user_id_to_carts.php

--- database/migrations/2022_03_27_170559_add_user_id_to_carts.php
+++ database/migrations/2022_03_27_170559_add_user_id_to_carts.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddUserIdToCart extends Migration
+class AddUserIdToCarts extends Migration
 {
     /**
      * Run the migrations.
