b35e6f0417ff732eeda9deacc88d87b1f21d7952
MRyousef1998 <75533977+MRyousef1998@users.noreply.github.com>
2022-06-03 15:51:05 +0300

    Update CartController.php

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace App\Http\Controllers\Api;
+use  App\Http\Resources\CategoryResource;
+use App\Http\Resources\ProductResource;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use  App\Cart;
+
+class CartController extends Controller
+{
+    
+        public function index(Request $request){
+            $user=Auth::user();
+            $cart=$this->conectionState($user->cart);
+
+        }
+    private function conectionState(Cart $cart){
+    
+    
+    }
+       
+
+
+}
