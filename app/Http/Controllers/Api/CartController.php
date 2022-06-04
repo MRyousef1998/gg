75c0fe75601b813ebf6c528781d5ff8e25776ec4
MRyousef1998 <75533977+MRyousef1998@users.noreply.github.com>
2022-06-04 03:20:54 +0300

    Update CartController.php

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -23,7 +23,7 @@ class CartController extends Controller
             $user=Auth::user();
             
             $product_id=$request->input('product_id');
-            $product=Product::findOrFall($product_id);
+            $product=Product::findOrFail($product_id);
             $cart=$user->cart;
             $qty=$request->input('qty');
             
