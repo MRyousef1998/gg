167c43b18db16e76198159ea1bccc3c1240c97e7
MRyousef1998 <75533977+MRyousef1998@users.noreply.github.com>
2022-06-04 05:21:33 +0300

    Update Cart.php

--- app/Cart.php
+++ app/Cart.php
@@ -38,7 +38,7 @@ class Cart extends Model
         } 
          else{
                if(! is_array($cartItems))
-           $cartItems=json_decode($this->cart_items,true);
+           $cartItems=json_decode($cartItems);
            }
          $cartItem=new CartItem($product,$qty); 
         // dd( $product);
