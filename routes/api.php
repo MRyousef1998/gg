b896772c932a6a6b4bd071f2f827287ba3fb7896
MRyousef1998 <75533977+MRyousef1998@users.noreply.github.com>
2022-06-10 16:52:20 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -55,6 +55,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::middleware('auth:api')->group( function () {
   Route::post('carts','Api\CartController@AddProductToCart');
+  Route::get('carts','Api\CartController@index');
 });
 
  
